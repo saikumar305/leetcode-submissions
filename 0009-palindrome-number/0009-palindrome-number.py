596a87ec0160cfa38cba1634a59731170c09641b
Rachakonda Sai kumar <saikumarrachakonda305@gmail.com>
2024-10-12 23:10:44 +0530

    Time: 54 ms (55.73%), Space: 16.6 MB (49.44%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -5,11 +5,9 @@ class Solution:
         reversed = 0
         temp = x
         while temp>0:
-            print(reversed)
             reversed = reversed*10 + temp%10
-            print("---", reversed)
             temp = temp//10
-        print(reversed)
+
         return x == reversed
 
         
