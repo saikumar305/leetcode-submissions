16720a1ecc622647e942e2354b62beab8a222ef3
Rachakonda Sai kumar <saikumarrachakonda305@gmail.com>
2024-12-03 00:49:31 +0530

    Time: 0 ms (100%), Space: 14.4 MB (5.03%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -13,6 +13,6 @@ class Solution(object):
         """
         if not root: return False
         if not root.left and not root.right and root.val == target: return True
-        if self.hasPathSum(root.left, target-root.val) or self.hasPathSum(root.right, target-root.val): return True
-        else: return False
+        return self.hasPathSum(root.left, target-root.val) or self.hasPathSum(root.right, target-root.val)
+
         
