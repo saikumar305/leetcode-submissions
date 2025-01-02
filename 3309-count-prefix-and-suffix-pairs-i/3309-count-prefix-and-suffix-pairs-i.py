b16791951d9993c3bb154af0e14aefa72301db1f
Rachakonda Sai kumar <saikumarrachakonda305@gmail.com>
2025-01-03 00:34:16 +0530

    Time: 8 ms (73.42%), Space: 17.8 MB (15.35%) - LeetHub

--- 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.py
+++ 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.py
@@ -8,6 +8,9 @@ class Solution:
                 str1 = words[i]
                 str2 = words[j]
 
+                if len(str1) > len(str2):
+                    continue 
+
                 if str2.startswith(str1) and str2.endswith(str1):
                     count+=1
 
