0aae7adcdb4f0a1af13c78e76c9291763a88643d
Rachakonda Sai kumar <saikumarrachakonda305@gmail.com>
2025-01-03 00:54:51 +0530

    Time: 3 ms (30.04%), Space: 17.7 MB (18.16%) - LeetHub

--- 3353-existence-of-a-substring-in-a-string-and-its-reverse/3353-existence-of-a-substring-in-a-string-and-its-reverse.py
+++ 3353-existence-of-a-substring-in-a-string-and-its-reverse/3353-existence-of-a-substring-in-a-string-and-its-reverse.py
@@ -3,7 +3,6 @@ class Solution:
         a=s[::-1]
         for i in range(1,len(s)):
             k=s[i-1:i+1]
-            print(k)
             if k in a:
                 return True
         return False
