c658be07c57b56dd6e5c85d7de8b095cfc91e564
Rachakonda Sai kumar <saikumarrachakonda305@gmail.com>
2025-04-24 01:59:27 +0530

    Time: 20 ms (58.88%), Space: 19.7 MB (26.78%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.py
+++ 0575-distribute-candies/0575-distribute-candies.py
@@ -1,9 +1,5 @@
 class Solution:
     def distributeCandies(self, candyType: List[int]) -> int:
-        hashmap = {}
-        for i in candyType:
-            hashmap[i] = hashmap.get(i,0)+1
-
-        return int(min(len(candyType) / 2 , len(hashmap)))
+        return min(len(set(candyType)), len(candyType)//2)
 
         
