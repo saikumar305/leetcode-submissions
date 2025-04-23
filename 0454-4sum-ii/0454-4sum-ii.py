fbffe59d5bcf8bf30f32430cebab8bef474af1bc
Rachakonda Sai kumar <saikumarrachakonda305@gmail.com>
2025-04-24 01:14:58 +0530

    Time: 340 ms (57.17%), Space: 17.9 MB (75.51%) - LeetHub

--- 0454-4sum-ii/0454-4sum-ii.py
+++ 0454-4sum-ii/0454-4sum-ii.py
@@ -1,14 +1,13 @@
 class Solution:
     def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
         count_map = defaultdict(int)
-        print(count_map)
+
     
         # Step 1: Store sums of all pairs from nums1 and nums2
         for a in nums1:
             for b in nums2:
                 count_map[a + b] += 1
 
-        print("nnw", count_map)
         result = 0
 
         # Step 2: For each pair in nums3 and nums4, check if -(c + d) exists
