5d57258e095715a935f77b992baedd42c70c644d
Rachakonda Sai kumar <saikumarrachakonda305@gmail.com>
2025-08-19 07:44:20 +0530

    Time: 46 ms (77.26%), Space: 26.8 MB (94.72%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,12 +1,24 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        maxi = 0
-        mini = prices[0];
-        n = len(prices);
+        # brute force
+        # curr_max = 0
+
+        # for i in range(len(prices)):
+        #     for j in range(i+1, len(prices)):
+        #         if prices[j] > prices[i]:
+        #             curr_max = curr_max if curr_max > prices[j] - prices[i] else prices[j] - prices[i]
+
+        buy = prices[0]
+        profit = 0
+
+        for i in range(1, len(prices)):
+            if prices[i] < buy:
+                buy = prices[i]
+            elif prices[i] - buy > profit:
+                profit = prices[i] - buy
+
+
+        return profit
+
+
         
-        for i in range(0,n):
-            mini = min(mini,prices[i])        
-            profit = prices[i]-mini
-            maxi = max(maxi,profit)
-            
-        return maxi
