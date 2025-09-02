cea6367cf54d8a88483618ec94bcd167c3b4a909
Rachakonda Sai kumar <saikumarrachakonda305@gmail.com>
2025-09-03 00:20:58 +0530

    Time: 3 ms (68.28%), Space: 18.9 MB (16.82%) - LeetHub

--- 0674-longest-continuous-increasing-subsequence/0674-longest-continuous-increasing-subsequence.py
+++ 0674-longest-continuous-increasing-subsequence/0674-longest-continuous-increasing-subsequence.py
@@ -6,13 +6,11 @@ class Solution:
 
         for i in range(len(nums)-1):
             if nums[i+1] > nums[i]:
-                print(max_len)
                 curr_max_len += 1
             else:
                 prev_max_len = curr_max_len
                 curr_max_len = 1
                 max_len = max(prev_max_len, max_len)
-        print(prev_max_len, curr_max_len)
         return max(curr_max_len, max_len)
                 
 
