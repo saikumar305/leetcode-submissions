2222fbd6d855e911cd55a5d50d290d09ab985dc5
Rachakonda Sai kumar <saikumarrachakonda305@gmail.com>
2026-03-15 17:52:21 +0530

    Time: 51 ms (60.16%), Space: 31.3 MB (98.75%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.py
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.py
@@ -1,16 +1,34 @@
 class Solution:
-    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+    # def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
 
-        arr.sort()
-        min_diff = float("inf")
+        # arr.sort()
+        # min_diff = float("inf")
 
-        for i in range(len(arr) -1):
+        # for i in range(len(arr) -1):
             
-            min_diff = min(min_diff , abs(arr[i+1] - arr[i]))
+        #     min_diff = min(min_diff , abs(arr[i+1] - arr[i]))
 
+        # res = []
+        # for i in range(len(arr) -1):
+        #     if abs(arr[i+1] - arr[i]) == min_diff:
+        #         res.append([arr[i], arr[i+1]])
+
+        # return res
+
+    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+        arr.sort()
+
+        min_diff = float("inf")
         res = []
-        for i in range(len(arr) -1):
-            if abs(arr[i+1] - arr[i]) == min_diff:
+
+        for i in range(len(arr) - 1):
+            diff = arr[i+1] - arr[i]
+
+            if diff < min_diff:
+                min_diff = diff
+                res = [[arr[i], arr[i+1]]]
+
+            elif diff == min_diff:
                 res.append([arr[i], arr[i+1]])
 
         return res
