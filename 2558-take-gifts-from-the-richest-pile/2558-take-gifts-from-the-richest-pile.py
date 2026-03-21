c8f49dbe465d5ff411e87d849e3607052788a326
Rachakonda Sai kumar <saikumarrachakonda305@gmail.com>
2026-03-21 13:37:41 +0530

    Time: 1 ms (88.42%), Space: 19.4 MB (95.47%) - LeetHub

--- 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
@@ -1,12 +1,26 @@
 class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
 
-        gifts.sort()
+        # gifts.sort()
+
+        # for i in range(k):
+        #     val = gifts.pop()
+        #     gifts.append(math.floor(val**0.5))
+        #     gifts.sort()
+
+        # return sum(gifts)
+
+        heap = [-g for g in gifts]
+
+        heapq.heapify(heap)
 
         for i in range(k):
-            val = gifts.pop()
-            gifts.append(math.floor(val**0.5))
-            gifts.sort()
+            largest = -heapq.heappop(heap)
+            value = int(math.isqrt(largest))
+            heapq.heappush(heap, -value)
+        
+        return -sum(heap)
 
-        return sum(gifts)
+
+
         
