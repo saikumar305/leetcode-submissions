38523b9750d2a13cc65e911865e9d370d703a65b
Rachakonda Sai kumar <saikumarrachakonda305@gmail.com>
2026-03-27 13:04:19 +0530

    Time: 52 ms (68.86%), Space: 22.5 MB (93.22%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -3,14 +3,17 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
+
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        nodes = set()
+        sp, fp = head, head
 
-        while head:
-            if head in nodes:
+        while fp and fp.next:
+            sp = sp.next
+            fp = fp.next.next
+
+            if sp == fp:
                 return True
-            nodes.add(head)
-            head = head.next
+
         return False
+        
