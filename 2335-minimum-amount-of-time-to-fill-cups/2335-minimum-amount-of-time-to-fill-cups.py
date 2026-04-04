9367bf36dfe977955dfc79f87ed0e938bd87ca94
Rachakonda Sai kumar <saikumarrachakonda305@gmail.com>
2026-04-04 19:07:37 +0530

    Time: 2 ms (35.45%), Space: 19.4 MB (50.18%) - LeetHub

--- 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.py
+++ 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.py
@@ -1,9 +1,26 @@
 class Solution:
     def fillCups(self, amount: List[int]) -> int:
 
-        return max(max(amount), math.ceil(sum(amount) / 2))
+        heap = [-a for a in amount if a>0]
+        heapq.heapify(heap)
 
+        time = 0
 
+        while heap:
+            first = -heapq.heappop(heap)
 
+            if heap:
+                second = -heapq.heappop(heap)
+                second -= 1
+                if second>0:
+                    heapq.heappush(heap, -second)
 
-        
+            first-=1
+            
+            if first>0:
+                    heapq.heappush(heap, -first )
+            
+            time+=1
+            
+        return time
+            
