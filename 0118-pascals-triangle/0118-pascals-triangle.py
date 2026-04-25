29988d09f134fa5265de6e20288939511b11f66e
Rachakonda Sai kumar <saikumarrachakonda305@gmail.com>
2026-04-25 12:37:46 +0530

    Time: 1 ms (13.24%), Space: 19.2 MB (93.21%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,14 +1,17 @@
-class Solution(object):
-    def generate(self, numRows):
-        """
-        :type numRows: int
-        :rtype: List[List[int]]
-        """
-        pascal = []
-        for i in range(0,numRows):
-            row = [1]*(i+1)
-            for j in range(1,i):
-                row[j] = pascal[i - 1][j - 1] + pascal[i - 1][j]
-            pascal.append(row)
-    
-        return pascal
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+
+        op = []
+        for i in range(numRows):
+
+            row = [1] * (i+1)
+            for j in range(1, i):
+                row[j] = op[i-1][j-1] +op[i-1][j]
+
+            op.append(row)
+
+
+        return op
+
+            
+        
