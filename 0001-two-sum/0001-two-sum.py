d1ac43012dae7b42871be19348ef6d19b29b9818
Rachakonda Sai kumar <saikumarrachakonda305@gmail.com>
2026-05-08 22:43:45 +0530

    Time: 0 ms (100%), Space: 20.5 MB (43.09%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+
         mapper = {}
 
-        for i in range(len(nums)):
-            if target - nums[i] in mapper:
-                return [i, mapper[target-nums[i]]]
+        for i, num in enumerate(nums):
+            if target - num in mapper:
+                return [i, mapper[target-num]]
+            else:
+                mapper[num] = i
 
-            mapper[nums[i]] = i
+        
+
         
