5b237a0feb5807a5e466551fd3cd8e486785568e
Rachakonda Sai kumar <saikumarrachakonda305@gmail.com>
2026-09-24 22:10:49 +0530

    Time: 4 ms (63.76%), Space: 19.3 MB (21.05%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,10 +1,23 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        roman={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
-        number=0
-        for i in range(len(s)-1):
-            if roman[s[i]] < roman[s[(i+1)]]:
-                number-=roman[s[i]]
+
+        mapper = {
+            "I" : 1,
+            "V" : 5,
+            "X" : 10,
+            "L" : 50,
+            "C" : 100,
+            "D" : 500,
+            "M" : 1000
+        }
+        val = 0
+
+        for  i in range(len(s)-1):
+            if (mapper[s[i+1]] > mapper[s[i]]):
+                val -= mapper[s[i]]
+
             else:
-                number+=roman[s[i]]
-        return number+roman[s[-1]]
+                val += mapper[s[i]]
+
+        return val + mapper[s[-1]]
+        
