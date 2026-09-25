9398e29b1b7348408b4554673edd4796db493892
Rachakonda Sai kumar <saikumarrachakonda305@gmail.com>
2026-09-25 15:46:05 +0530

    Time: 62 ms (10.29%), Space: 45 MB (52.73%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
@@ -1,29 +1,28 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def minDepth(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: int
-        """
-        if root is None:  return 0
-        # Initialize the depth of two subtrees...
-        leftDepth = self.minDepth(root.left)
-        rightDepth = self.minDepth(root.right)
-        # If the both subtrees are empty...
+class Solution:
+    def minDepth(self, root: TreeNode | None) -> int:
+
+        if root is None:return 0
+
+        left_depth = self.minDepth(root.left)
+        right_depth = self.minDepth(root.right)
+
+
         if root.left is None and root.right is None:
             return 1
-        # If the left subtree is empty, return the depth of right subtree after adding 1 to it...
-        if root.left is None:
-            return 1 + rightDepth
-        # If the right subtree is empty, return the depth of left subtree after adding 1 to it...
-        if root.right is None:
-            return 1 + leftDepth
-        # When the two child function return its depth...
-        # Pick the minimum out of these two subtrees and return this value after adding 1 to it...
-        return min(leftDepth, rightDepth) + 1; 
+
+        if root.right and root.left is None:
+            return 1 + right_depth
+
+        if root.left and root.right is None:
+            return 1 + left_depth
+
+        return min(left_depth, right_depth) +1
+        
+
         
